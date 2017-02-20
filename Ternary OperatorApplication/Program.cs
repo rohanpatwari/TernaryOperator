edb02d541c458b33b8a4ac4627c36665a3bd0d50
rohan <rohan@DESKTOP-2911EVS>
2017-02-20 14:55:21 -0600

    Add project files.

---
+++ Ternary OperatorApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_OperatorApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string name1 = "Rohan";
+        string name2 = "Ram";
+        var result = name1.Length < 5?name1:name2;
+        Console.WriteLine(result);
+        Console.ReadLine();
+        }
+    }
+}
